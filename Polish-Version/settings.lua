28f83690238b3e9f4563e17847c4f0bb8f7471ab
lukasz4444 <93074713+lukasz4444@users.noreply.github.com>
2022-11-16 17:49:51 +0100

    Delete settings.lua

--- Polish-Version/settings.lua
+++
@@ -1,7 +0,0 @@
-local settings = {
-    usedhd = false , --czy chcesz wybrać adres za pomocą dhd (Ostrzeżenie jest to funkcja eksperymentalna).
-    justone = false , -- tylko jeden obiekt może przejść przez gwiezdne wrota, po czym się wyłączą.
-    dorestart = true -- Restartować?
-}
-
-return settings
